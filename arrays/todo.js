62d69d17932683d92089c8aedf22dc837b728def
Matt <matthewgarrity85@mac.com>
2018-10-02 08:48:03 +0100

    Array exercises

--- arrays/todo.js
+++ arrays/todo.js
@@ -15,13 +15,37 @@ const todos = [{
     completed: true
 }]
 
+const sortTodos = function(todos) {
+    todos.sort(function(a, b) {
+        if (a.completed === true && b.completed === false) {
+            return 1
+        } else if (a.completed === false && b.completed === true) {
+            return -1
+        } else {
+            return 0
+        }
+    })
+}
+
 const deleteTodo = function(todos, task) {
-    const index = todos.findIndex( function (todo, index) {
+    const index = todos.findIndex(function (todo, index) {
         return todo.text === task
     })
     return todos.splice(index, 1)
 }
 
-deleteTodo(todos, 'Play guitar')
-console.log(todos)
+// deleteTodo(todos, 'Play guitar')
+// console.log(todos)
 
+const getThingsToDo = function(todos) {
+    return filteredTodos = todos.filter( function(todo){
+        const isCompleted = todo.completed
+        return isCompleted
+    })
+}
+
+// console.log(getThingsToDo(todos))
+
+
+sortTodos(todos)
+console.log(todos)
